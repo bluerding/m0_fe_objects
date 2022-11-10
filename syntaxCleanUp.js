2739e464cf41cdbca0db19082bae03fedd200763
Ben Luerding <bluerding@gmail.com>
2022-11-10 16:10:15 -0600

    add cleanup

--- syntaxCleanUp.js
+++ syntaxCleanUp.js
@@ -1,22 +1,30 @@
-var foods = {apples: 23, 
+var foods = {
+  apples: 23, 
   grapes: 507, 
-  eggs: 48}
-
-var person = { height: "6 ft", weight: "160 lbs" }
-
-var post = { imageSrc: "./images/beach.png",
-
-
-   caption: "At the beach with my besties", timestamp: "4:37 PM August 13, 2019",
-        number_likes: 0,
-comments: [] }
-
-
-var post2 =
-{
-      imageSrc: "./images/holiday-party.png",
-      caption: "What a great holiday party omg",
-      timestamp: "11:37 PM December 31, 2019",
-      numberLikes: 13,
-      comments: []
+  eggs: 48
 }
+//Moved apples to line 2. Moved closing bracket to line 5
+
+var person = { 
+  height: "6 ft", 
+  weight: "160 lbs" 
+}
+//Moved height to line 9, then weight to line 10. Moved clsoing bracket to line 11
+
+var post = { 
+  imageSrc: "./images/beach.png",
+  caption: "At the beach with my besties", 
+  timestamp: "4:37 PM August 13, 2019",
+  number_likes: 0,
+  comments: [] 
+}
+//Moved each key value to its own line, and lined them up with one indent. Moved closing bracket to its own line
+
+var post2 = {
+  imageSrc: "./images/holiday-party.png",
+  caption: "What a great holiday party omg",
+  timestamp: "11:37 PM December 31, 2019",
+  numberLikes: 13,
+  comments: []
+}
+//Moved the opening bracket to line 23. Removed excessive indentations on each key value
